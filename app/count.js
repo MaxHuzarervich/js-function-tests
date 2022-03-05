8d4a2ca6e21080ac67a1ca8a24ce7624393496ee
MaxHuzarevich <pinsk.maks@mail.ru>
2022-03-05 23:11:34 +0300

    count.js refactor completed

--- app/count.js
+++ app/count.js
@@ -1,7 +1,21 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.countAnswers = {
-  count: function (start, end) {
+    count: function (start, end) {
+        var timeOut;
 
-  }
+        function counter() {
+            console.log(start++);
+            if (start <= end) {
+                timeOut = setTimeout(counter, 100);
+            }
+        }
+
+        counter();
+        return {
+            cancel: function () {
+                timeOut && clearTimeout(timeOut);
+            }
+        };
+    }
 };
